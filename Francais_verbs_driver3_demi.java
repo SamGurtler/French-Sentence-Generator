6b2e3f698881cf669c1dacb9efde094dc9b23c08
SamGurtler <StudentcareerSamgurtler@gmail.com>
2016-08-26 10:21:15 -0700

    Update Francais_verbs_driver3_demi.java

--- Francais_verbs_driver3_demi.java
+++ Francais_verbs_driver3_demi.java
@@ -25,11 +25,29 @@ import java.util.Random;
 		}
 		System.gc();
 		int rand = new Random.nextInt(1);
-		int rand1 = new Random.nextInt(6);
+		int rand1 = new Random.nextInt(8);
+		int rand2 = new Random.nextInt(6);
+		int conjugation_index = rand1;
+		if(rand1 = 2||rand1 = 3||rand1 = 4){
+			conjugation_index = 2;
+		}
+		if(rand1=5){
+			conjugation_index = 3;
+		}
+		if(rand1=6){
+			conjagation_index = 4;
+		}
+		if(rand1=7||rand1=8){
+			conjugation_index = 5;
+		}
 		String[] PronounSubject = {"Je","Tu","Il","Elle","On","Nous","Vous","Ils","Elles"}
+		//                           0    1    2    3     4     5      6      7     8
+		//                           0    1    2    2     2     3      4      5     5
 		switch(rand){
 		case 0:
 			//If statments inside here maybe
+			
+			System.out.println(PronounSubject[rand1]+" "+VerbList[rand2].getVerb[conjugation_index]);
 			break;
 		case 1:
 			//If statments inside here maybe
